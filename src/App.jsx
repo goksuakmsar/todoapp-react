fe13bc5e7edf8a553020e13646e3cadf51b816b7
Göksu Akımsar <goksuakimsar@Goksu-MacBook-Air.local>
2023-11-23 11:14:19 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-// import Inputvalue from "./components/input-value";
-// import Life from "./components/yasam-dongusu";
-// import { useState } from 'react'
-
 import ToDo from "./components/ToDo";
 
-
 function App() {
-  // const [show, setshow] = useState(false)
 
   return (
     <>
-      {/* <button onClick={() => setshow(!show)}>{show ? 'Gizle' : 'Göster'}</button> */}
 
-      {/* {
-        (show === true) &&
-        <Life />
-      } */}
-
-      {/* <Inputvalue /> */}
       <div className="container mt-5">
         <ToDo />
       </div>
-
-
-
-
-
 
     </>
   );
